a93c0bf751612ad9df3e2cf011b09865ae70e84f
Subham Kumar <subhamk1978@gmail.com>
2024-09-07 15:11:12 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -117,7 +117,7 @@ export default function Home() {
                 <p className="hidden sm:flex mt-3 text-lg lg:text-xl lg:font-medium">
                   Delhi, India’s capital territory, is a massive metropolitan area in the country’s north. In Old Delhi, a neighborhood dating to the 1600s, stands the imposing Mughal-era Red Fort, a symbol of India, and the sprawling Jama Masjid mosque, whose courtyard accommodates 25,000 people.
                 </p>
-                <p className="sm:hidden">
+                <p className="sm:hidden text-sm">
                   Delhi, India’s capital territory, is a massive metropolitan area in...
                 </p>
                 <button className="mt-4">
